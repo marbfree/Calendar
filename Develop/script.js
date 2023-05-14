c3f9e79c8f64ee15f297aa902603002a7b4efc9e
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-13 20:54:31 -0400

    added event listener to save buttons

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,19 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
+  let saveBtn = $('.btn');
+  saveBtn.on('click', function(){
+    // console.log('button clicked');
+    let textInputValue = $('textarea[rows = "3"]').val();
+    console.log(textInputValue);
+    localStorage.setItem("textInput", textInputValue);
+    console.log(textInputValue);
+    localStorage.getItem("textInput");
+    alert(localStorage.getItem("textInput"));
+  });
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
