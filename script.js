aa3d6fb7d4fc8a6048a4f7d65ce318d9e177c586
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-17 18:23:05 -0400

    added for loop for get local Storage

--- script.js
+++ script.js
@@ -38,16 +38,24 @@ $(document).ready(function () {
   // runs the updateHours function
   updateHours();
 
+var allTextareas = $('.description')
+  for (let i=0; i < allTextareas.length; i++){
+    var parentID = $(allTextareas[i]).parent().attr("id");
+    $(allTextareas[i]).val(localStorage.getItem(parentID));
+    console.log(parentID);
+  }
+
+
   // Gets the local storage to display text from textarea upon refresh
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  // $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  // $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  // $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  // $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  // $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  // $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  // $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  // $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  // $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
   // displays the current date in the header of the page.
   $('#currentDay').append(dayjs().format('MMMM-D-YYYY'));
